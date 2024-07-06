5fd90e0f05ab91123d3c8c18f128d26337bf3505
bellamoss77 <mmf295@icloud.com>
2024-07-05 23:42:57 -0400

    bringing gh up to date

--- src/components/LandscapeGallery.js
+++ src/components/LandscapeGallery.js
@@ -1,75 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import { Responsive, WidthProvider } from 'react-grid-layout';
+import PhotoAlbum from 'react-photo-album';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/styles.css'
 import './LandscapeGallery.scss';
+import { event } from 'jquery';
 
-const ResponsiveGridLayout = WidthProvider(Responsive);
+
 
 const images =  [
     {
-        src: '/LANDSCAPE-IMGS/Hikes-Late-May24_6.png', 
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/Hikes-Late-May24_6.png', 
+        width: 800,
+        height: 600,
         alt: 'hilly landscape bathed in light',
         caption: 'Walnut Creek Park, North Garden, VA - Late May 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/Hikes-Late-May24_10.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/Hikes-Late-May24_10.png',
+        width: 800,
+        height: 600,
         alt: 'Sunset reflection on water',
         caption: 'Walnut Creek Park, North Garden, VA - Late May 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/JRSP_June1-24_11.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/JRSP_June1-24_11.png',
+        width: 800,
+        height: 600,
         alt: 'riverbed underwater',
         caption: 'James River State Park, Gladstone, VA - June 1, 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/JSRP_May-31-24_1.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/JRSP_May-31-24_1.png',
+        width: 800,
+        height: 600,
         alt: 'wheatgrass strand',
         caption: 'James River State Park, Gladstone, VA - May 31, 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/JSRP_May-31-24_18.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/JRSP_May-31-24_18.png',
+        width: 800,
+        height: 600,
         alt: 'purple flower against bokeh background',
         caption: 'James River State Park, Gladstone, VA - May 31, 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/JRSP_May-31-24_14.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/JRSP_May-31-24_14.png',
+        width: 800,
+        height: 600,
         alt: 'wheatgrass sways in breeze in front of marsh',
         caption: 'James River State Park, Gladstone, VA - May 31, 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/JRSP_May-31-24_26.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/JRSP_May-31-24_26.png',
+        width: 800,
+        height: 600,
         alt: 'sunset over marsh',
         caption: 'James River State Park, Gladstone, VA - May 31, 2024'
      }, {
-        src: 'LANDSCAPE-IMGS/skylineSunset1-05.png',
+        src: 'https://github.com/bellamoss77/react-photog-portfolio/blob/main/public/LANDSCAPE-IMGS/skylineSunset1-05.png?raw=true',
+        width: 800,
+        height: 600,
         alt: 'vivid sunset against tree silhouette',
         caption: 'Blue Ridge Parkway Sunset Near Rockfish Gap, VA - April 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/skylineSunset1-11.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/skylineSunset1-11.png',
+        width: 800,
+        height: 600,
         alt: 'subdued sunset against mountain',
         caption: 'Blue Ridge Parkway Sunset Near Rockfish Gap, VA - April 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_1.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_1.png',
+        width: 800,
+        height: 600,
         alt: 'sunrise from blue ridge parkway',
         caption: 'Blue Ridge Parkway Sunrise Near Rockfish Gap, VA - Late June 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_2.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_2.png',
+        width: 800,
+        height: 600,
         alt: 'sunrise from blue ridge parkway',
         caption: 'Blue Ridge Parkway Sunrise Near Rockfish Gap, VA - Late June 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_5.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_5.png',
+        width: 800,
+        height: 600,
         alt: 'sunrise from blue ridge parkway',
         caption: 'Blue Ridge Parkway Sunrise Near Rockfish Gap, VA - Late June 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_9.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNRISE_9.png',
+        width: 800,
+        height: 600,
         alt: 'sunrise from blue ridge parkway',
         caption: 'Blue Ridge Parkway Sunrise Near Rockfish Gap, VA - Late June 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNSET_1.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNSET_1.png',
+        width: 600,
+        height: 800,
         alt: 'woman looks out at sunset from Ravens Roost, Blue Ridge Parkway, VA',
         caption: "Sunset from Raven's Roost Overlook, Blue Ridge Parkway, VA - Late June 2024"
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNSET_11.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNSET_11.png',
+        width: 800,
+        height: 600,
         alt: 'vivid sunset in central va',
         caption: 'Sunset in Central Virginia - Mid June 2024'
      }, {
-        src: '/LANDSCAPE-IMGS/SKYLINE-SUNSET_14.png',
+        src: process.env.PUBLIC_URL + '/LANDSCAPE-IMGS/SKYLINE-SUNSET_14.png',
+        width: 800,
+        height: 600,
         alt: 'sunset from blue ridge parkway',
         caption: "Sunset from Raven's Roost Overlook, Blue Ridge Parkway, VA - Late June 2024"
      }
@@ -79,15 +112,6 @@ const LandscapeGallery = () => {
     const [open, setOpen] = useState(false);
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    
-    const layout = images.map((image, index) => ({
-        i: image.src,
-        x: (index % 3) * 4,
-        y: Math.floor(index / 3) * 4,
-        w: 4,
-        h: 4
-    }));
-
     const handleOpen = (index) => {
         setCurrentImageIndex(index);
         setOpen(true);
@@ -95,25 +119,11 @@ const LandscapeGallery = () => {
 
     return (
         <div className='gallery'>
-            <ResponsiveGridLayout
-            className='layout'
-            layouts={{ lg: layout }}
-            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-            rowHeight={30}
-            >
-                {images.map((image, index) => (
-                    <div key={image.src} data-grid={layout[index]}>
-                        <img
-                          src={image.src}
-                          alt={image.alt}
-                          onClick={() => handleOpen(index)}
-                          style={{ cursor: 'pointer', width: '100%', height: 'auto' }}
-                        />
-                    </div>
-                ))}
-            </ResponsiveGridLayout>
-
+            <PhotoAlbum
+              layout='rows'
+              photos={images}
+              onClick={({ index }) => handleOpen(index)}
+            />
             {open && (
                 <Lightbox
                   open={open}
@@ -128,3 +138,5 @@ const LandscapeGallery = () => {
 };
 
 export default LandscapeGallery;
+
+
